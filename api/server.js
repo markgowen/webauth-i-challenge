04723724ee3bfd647c6a4b63152414d0ff4be730
Mark Gowen <mark.gowen78@gmail.com>
2019-11-11 12:43:57 -0800

    created auth-middleware

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const apiRouter = require('./api-router');
+const authRouter = require('./auth/auth-router');
 const configMW = require('./middleware/config-mw');
 
 const server = express();
 
 configMW(server);
 
-server.use(('/api', apiRouter));
+server.use(('/auth', authRouter));
 
 module.exports = server;
