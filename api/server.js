b77baaa4ed5ce93b5f9f3817f43cc392a804bacf
Mark Gowen <mark.gowen78@gmail.com>
2019-11-11 12:11:36 -0800

    initial file structure built

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const apiRouter = require('./api-router');
+const configMW = require('./middleware/config-mw');
+
+const server = express();
+
+configMW(server);
+
+server.use(('/api', apiRouter));
+
+module.exports = server;
