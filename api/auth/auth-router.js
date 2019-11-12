b5a14369d93202f0587f37f963ca3aa712ff707a
Mark Gowen <mark.gowen78@gmail.com>
2019-11-12 10:27:36 -0800

    added express session and restricted middleware

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,42 +1,43 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Users = require('../users/user-model');
+const Users = require("../users/user-model");
 
-router.post('/register', (req, res) => {
-    let userInfo = req.body;
+router.post("/register", (req, res) => {
+  let userInfo = req.body;
 
-    bcrypt.hashSync(userInfo.password, 12, (err, hashedPassword) => {
-        userInfo.password = hashedPassword;
+  bcrypt.hashSync(userInfo.password, 12, (err, hashedPassword) => {
+    userInfo.password = hashedPassword;
 
-        Users.insert(userInfo)
-            .then(saved => {
-                res.status(201).json(saved);
-            })
-            .catch(err => {
-                console.log('Error', err);
-                res.status(500).json(err);
-            });
-    });
+    Users.insert(userInfo)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(err => {
+        console.log("Error", err);
+        res.status(500).json(err);
+      });
+  });
 });
 
-router.post('/login', (req, res) => {
-    let { username, password } = req.body;
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
 
-    Users.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                res.status(200).json({ message: `Logged in ${user.user_id}` });
-            } else {
-                res.status(401).json({ message: 'You shall not pass!' });
-            }
-        })
-        .catch(err => {
-            console.log('Error...', err);
-            res.status(500).json(err);
-        });
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
+        res.status(200).json({ message: `Logged in ${user.user_id}` });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(err => {
+      console.log("Error...", err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,14 +1,26 @@
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
 
-const apiRouter = require('./api-router')
-const configMW = require('./middleware/config-mw');
+const apiRouter = require("./api-router");
+const configMW = require("./middleware/config-mw");
+
+const sessionConfig = {
+  name: "monster",
+  secret: process.env.COOKIE_SECRET || "is it secret? is it safe?",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: process.env.NODE_ENV === "development" ? false : true,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: true
+};
 
 const server = express();
 
 configMW(server);
 
-server.use('/api', apiRouter);
-
-
+server.use(session(sessionConfig));
+server.use("/api", apiRouter);
 
 module.exports = server;
